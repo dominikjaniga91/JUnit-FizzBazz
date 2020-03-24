ccd1a4baa4af6f5993c7adfd1c517a904242cae3
Dominik <dominikjaniga91@gmail.com>
2020-03-24 14:28:39 +0100

    adding test that check if method returns fizz when number contains3

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -13,16 +13,22 @@ public class FizzBuzz {
 
     private void add100NumbersToList(List<String> listOfNumbers){
 
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 0; i <= 100; i++) {
             if(i % 5 == 0 && i % 3 == 0){
                 listOfNumbers.add("FizzBuzz");
             }else if( i % 5 == 0) {
                 listOfNumbers.add("Buzz");
-            }else if(i % 3 == 0){
+            }else if(i % 3 == 0 || ifContains3(i)){
                 listOfNumbers.add("Fizz");
             }else{
                 listOfNumbers.add(String.valueOf(i));
             }
         }
     }
+
+    private boolean ifContains3(int number){
+
+        return String.valueOf(number).contains("3");
+    }
+
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,4 +1,6 @@
 import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -12,33 +14,49 @@ public class FizzBuzzTest {
     @Test
     void shouldReturnFirstNumberOfTableEquals1(){
 
-        assertThat(fizzBuzz.getListOfNumbers().get(0)).isEqualTo("1");
+        assertThat(fizzBuzz.getListOfNumbers().get(1)).isEqualTo("1");
     }
 
     @Test
     void shouldReturnSizeOfListEquals100(){
 
-        assertThat(fizzBuzz.getListOfNumbers().size()).isEqualTo(100);
+        assertThat(fizzBuzz.getListOfNumbers().size()).isEqualTo(101);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {3,6,9,12,18,21,24,27,33})
-    void shouldReturnFizz_whenElementOfListIsDivisibleBy3(int numbers){
+    void shouldReturnFizz_whenNumberIsDivisibleBy3(int numbers){
 
-        assertThat(fizzBuzz.getListOfNumbers().get(numbers-1)).isEqualTo("Fizz");
+        assertThat(fizzBuzz.getListOfNumbers().get(numbers)).isEqualTo("Fizz");
     }
 
     @ParameterizedTest
     @ValueSource(ints = {5,10,20,25,35,40,50,55})
-    void shouldReturnBuzz_whenElementOfListIsDivisibleBy5(int number){
+    void shouldReturnBuzz_whenNumberIsDivisibleBy5(int number){
 
-        assertThat(fizzBuzz.getListOfNumbers().get(number-1)).isEqualTo("Buzz");
+        assertThat(fizzBuzz.getListOfNumbers().get(number)).isEqualTo("Buzz");
     }
 
     @ParameterizedTest
     @ValueSource(ints = {15,30,45,60,75})
-    void shouldReturnFizzBuzz_whenElementOfListIsDivisibleBy3And5(int number){
+    void shouldReturnFizzBuzz_whenNumberIsDivisibleBy3And5(int number){
 
-        assertThat(fizzBuzz.getListOfNumbers().get(number-1)).isEqualTo("FizzBuzz");
+        assertThat(fizzBuzz.getListOfNumbers().get(number)).isEqualTo("FizzBuzz");
+    }
+
+    @ParameterizedTest(name = "Number {0} should return Fizz")
+    @ValueSource(ints = {13,23,43})
+    @DisplayName("Should return fizz when number contains 3")
+    void shouldReturnFizz_whenNumberContains3(int number) {
+
+        assertThat(fizzBuzz.getListOfNumbers().get(number)).isEqualTo("Fizz");
+    }
+
+    @ParameterizedTest(name = "Number {0} should return Fizz")
+    @ValueSource(ints = {51,52,54,56})
+    @DisplayName("Should return buzz when number contains 5")
+    void shouldReturnBuzz_whenNumberContains5(int number) {
+
+        assertThat(fizzBuzz.getListOfNumbers().get(number)).isEqualTo("Fizz");
     }
 }
